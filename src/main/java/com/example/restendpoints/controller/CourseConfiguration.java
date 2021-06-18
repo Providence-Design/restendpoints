d17f987d01bc06d93da5ec5a7148610f24fd1753
Providence-Design <baabaprovidence@gmail.com>
2021-06-18 10:55:34 +0000

    error fixed

--- src/main/java/com/example/restendpoints/controller/CourseConfiguration.java
+++ src/main/java/com/example/restendpoints/controller/CourseConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("/course")
+@ConfigurationProperties("course")
 
 public class CourseConfiguration {
     private String name;
